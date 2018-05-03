14bdc075b10998a6bcac8f8210cb8ef8ca8a099a
Kaui Chu <smailzhu01@gmail.com>
2018-05-03 21:56:30 +0800

    Add commit of os.environ

--- kaui_bot.py
+++ kaui_bot.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#Ka-ui Chu 2018/05/03 @Taiwan
 import os
-import telepot
 import time
+import telepot
 from telepot.loop import MessageLoop
 from pprint import pprint
 
-TOKEN = os.environ.get('BOT_TOKEN')
-CHANNEL_ID = os.environ.get('FORWARD_CHANNEL_ID')
+TOKEN = os.environ.get('BOT_TOKEN') 
+#os.environ["BOT_TOKEN"] = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
+CHANNEL_ID = os.environ.get('FORWARD_CHANNEL_ID') 
+#os.environ["FORWARD_CHANNEL_ID"] = "CHANEEL_ID"
 def handle(msg):
     content_type, chat_type, chat_id, date, message_id = telepot.glance(msg, long=True)
     # http://telepot.readthedocs.io/en/latest/reference.html#telepot.Bot.forwardMessage
+    bot.forwardMessage(CHANNEL_ID, msg['from']['id'], message_id, disable_notification=True)
 
 bot = telepot.Bot(TOKEN)
 
