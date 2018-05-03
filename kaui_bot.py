5bd7007b417661c6605ba4bd3d4532c431fb8e23
Kaui Chu <smailzhu01@gmail.com>
2018-05-03 22:45:11 +0800

    Add bot info dialog
    add bot info message
    and
    check token and Channel id before launch

--- kaui_bot.py
+++ kaui_bot.py
@@ -12,6 +12,10 @@ TOKEN = os.environ.get('BOT_TOKEN')
 #os.environ["BOT_TOKEN"] = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
 CHANNEL_ID = os.environ.get('FORWARD_CHANNEL_ID') 
 #os.environ["FORWARD_CHANNEL_ID"] = "CHANEEL_ID"
+if TOKEN==None or CHANEEL_ID==None:
+    print("Please set Bot Token and Channel ID in os varialbe first")
+    exit()
+
 def handle(msg):
     content_type, chat_type, chat_id, date, message_id = telepot.glance(msg, long=True)
     # http://telepot.readthedocs.io/en/latest/reference.html#telepot.Bot.forwardMessage
@@ -20,7 +24,7 @@ def handle(msg):
 bot = telepot.Bot(TOKEN)
 
 MessageLoop(bot, handle).run_as_thread()
-print('Listening ...')
+print( '%s (@%s) is Listening ...' % (bot.getMe()['first_name'], bot.getMe()['username']) )
 # Keep the program running.
 while 1:
     time.sleep(10)
